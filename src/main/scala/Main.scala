29b18269c7c7060e2215e07b7673e2aa76ab8125
ko-kamenashi <kamenashi0917@gmail.com>
2020-09-18 19:47:58 +0900

    update codes to avoid stack overflow exception

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,10 @@ import scala.annotation.tailrec
 object Main {
   @tailrec
   def series(n: Int, acc: Int): Int = {
-    if (n == 0) 0 else series(n - 1 , acc + n)
+    if (n == 0) 0 else series(n - 1, acc + n)
   }
+
+  // Begginer's Challenge
+  @tailrec
+  def fact(n: Int, acc: Int): Int = if (n <= 1) 1 else fact(n - 1, acc * n)
 }
